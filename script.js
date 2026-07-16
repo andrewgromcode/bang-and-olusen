ed7ecd374bf615056849b82f1d4ea6191ef09490
Andrew Grom <hello@andrew-grom.com>
2026-07-16 23:24:42 +0300

    Fix phone icon hide on menu open and remove phone number underline

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const menuToggle = document.getElementById('menuToggle');
 const mobileMenu = document.getElementById('mobileMenu');
+const header = document.querySelector('.header');
 
 menuToggle.addEventListener('click', () => {
   mobileMenu.classList.toggle('is-open');
   menuToggle.classList.toggle('is-open');
+  header.classList.toggle('menu-open');
 });
 
 mobileMenu.querySelectorAll('a').forEach((link) => {
   link.addEventListener('click', () => {
     mobileMenu.classList.remove('is-open');
     menuToggle.classList.remove('is-open');
+    header.classList.remove('menu-open');
   });
 });
